3de7c432415b8a37183d880c210883e0925f038e
Yury Usishchev <yury.usischev@phystech.edu>
2017-02-14 08:36:59 +0300

    Patch pip in rootfs to fix python segfault in zabbix build

--- devenv/prep.sh
+++ devenv/prep.sh
@@ -22,6 +22,33 @@ chr apt-get install -y devscripts python-virtualenv equivs build-essential \
 # install git from backports to support desktop latest Git configs
 chr apt-get install -y -t wheezy-backports git git-man
 
+echo > $ROOTFS_DIR/pip_fix.patch <<EOF
+diff --git a/index.py b/index.py
+index 8e53e44..e4e9755 100644
+--- a/index.py
++++ b/index.py
+@@ -222,13 +222,7 @@ class PackageFinder(object):
+         done = []
+         seen = set()
+         threads = []
+-        for i in range(min(10, len(locations))):
+-            t = threading.Thread(target=self._get_queued_page, args=(req, pending_queue, done, seen))
+-            t.setDaemon(True)
+-            threads.append(t)
+-            t.start()
+-        for t in threads:
+-            t.join()
++        self._get_queued_page(req, pending_queue, done, seen)
+         return done
+ 
+     _log_lock = threading.Lock()
+EOF
+
+chr bash -c "cd /usr/share/pyshared/pip/ &&  patch -p1 < /pip_fix.patch"
+chr bash -c "cd /usr/share/python-virtualenv/ && tar -xvf pip-1.1.debian1.tar.gz && cd pip-1.1/pip &&  patch -p1 < /pip_fix.patch && cd /usr/share/python-virtualenv/ &&  tar -czvf  pip-1.1.debian1.tar.gz  pip-1.1 && rm -rf pip-1.1"
+
+rm $ROOTFS_DIR/pip_fix.patch
+
 (rm -rf $ROOTFS_DIR/dh-virtualenv && cd $ROOTFS_DIR && git clone https://github.com/spotify/dh-virtualenv.git && cd dh-virtualenv && git checkout 0.10)
 chr bash -c "cd /dh-virtualenv && mk-build-deps -ri && dpkg-buildpackage -us -uc -b"
 chr bash -c "dpkg -i /dh-virtualenv_*.deb"
